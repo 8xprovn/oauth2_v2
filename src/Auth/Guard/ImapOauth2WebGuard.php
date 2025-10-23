fcda5e25271f7811f8c2431a9626618600d9e918
@hiepcm <hiepcm@imap.edu.vn>
2025-10-23 11:23:14 +0700

    update version

--- src/Auth/Guard/ImapOauth2WebGuard.php
+++ src/Auth/Guard/ImapOauth2WebGuard.php
@@ -136,10 +136,17 @@ class ImapOauth2WebGuard
             $credentials['access_token'] = request()->header($this->cookePrefix . 'access_token');
             Cookie::queue($this->cookePrefix . 'access_token', $credentials['access_token'], 1440, null, null, true, false);
         }
+
         if (empty($credentials) && request()->hasHeader($this->cookePrefix . 'refresh_token')) {
             $credentials['refresh_token'] = request()->header($this->cookePrefix . 'refresh_token');
             Cookie::queue($this->cookePrefix . 'refresh_token', $credentials['refresh_token'], 8640, null, null, true, false);
         }
+
+        if (empty($credentials) && request()->has($this->cookePrefix . 'access_token')) {
+            $credentials['access_token'] = request()->get($this->cookePrefix . 'access_token');
+            Cookie::queue($this->cookePrefix . 'access_token', $credentials['access_token'], 1440, null, null, true, false);
+        }
+
 
         if (empty($credentials['access_token'])) {
             return false;
